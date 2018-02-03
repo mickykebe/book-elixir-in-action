d879dd733956e284aaef351be2e988c9ad90b7ea
mickykebe <mickykebe@gmail.com>
2018-02-03 15:21:32 +0300

    ch8

---
+++ ch8/lib/todo/database.ex
@@ -0,0 +1,52 @@
+defmodule Todo.Database do
+  use GenServer
+
+  def start_link(db_folder) do
+    IO.puts "Starting database server."
+    GenServer.start_link(__MODULE__, db_folder, name: :database_server)
+  end
+
+  def store(key, data) do
+    GenServer.cast(:database_server, {:store, key, data})
+  end
+
+  def get(key) do
+    GenServer.call(:database_server, {:get, key})
+  end
+
+  def init(db_folder) do
+    File.mkdir_p(db_folder)
+    worker_list = Enum.reduce(0..2, Map.new, fn(key, acc) ->
+      {:ok, worker_pid} = Todo.DatabaseWorker.start_link(db_folder)
+      Map.put(acc, key, worker_pid)
+     end)
+    {:ok, worker_list}
+  end
+
+  def handle_cast({:store, key, data}, worker_list) do
+    worker_pid = get_worker(key, worker_list)
+    Todo.DatabaseWorker.store(worker_pid, key, data)
+    
+    {:noreply, worker_list}
+  end
+
+  def handle_call({:get, key}, _, worker_list) do
+    worker_pid = get_worker(key, worker_list)
+
+    # don't know which is the right answer
+
+    # spawn(fn ->
+    #   data = Todo.DatabaseWorker.get(worker_pid, key)
+    #   GenServer.reply(caller, data)
+    # end)
+    # {:noreply, worker_list}
+    data = Todo.DatabaseWorker.get(worker_pid, key)
+    {:reply, data, worker_list}
+    
+  end
+
+  defp get_worker(key, worker_list) do
+    worker_map_key = :erlang.phash2(key, 3)
+    Map.get(worker_list, worker_map_key)
+  end
+end
---
+++ ch8/lib/todo/server.ex
@@ -0,0 +1,29 @@
+defmodule Todo.Server do
+  use GenServer
+
+  def start_link(name) do
+    IO.puts("Starting to-do server for #{name}")
+    GenServer.start_link(Todo.Server, name)
+  end
+  def add_entry(pid, entry) do
+    GenServer.cast(pid, {:add_entry, entry})
+  end
+  def entries(pid, date) do
+    GenServer.call(pid, {:entries, date})
+  end
+
+  def init(name) do
+    {:ok, {name, Todo.Database.get(name) || Todo.List.new()}}
+  end
+
+  def handle_cast({:add_entry, entry}, { name, todo_list}) do
+    new_state = Todo.List.add_entry(todo_list, entry)
+    Todo.Database.store(name, new_state)
+    {:noreply, {name, new_state}}
+  end
+
+  def handle_call({:entries, date}, _, { _, todo_list} = state) do
+    {:reply, Todo.List.entries(todo_list, date), state}
+  end
+
+end
